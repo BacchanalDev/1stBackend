ae7ea7da2d4aca26389a12d35a389fc773ed8d1b
King Kong <seanjohn0131@aol.com>
2020-07-16 13:38:37 -0400

    Making sure greet and calc runs right

--- calc/app.py
+++ calc/app.py
@@ -11,33 +11,61 @@ def add():
     '''It take 2 parameter and adds them
     >>> 4 + 5 = 9
     '''
-    a=int(request.args.get("a"))
-    b = int(request.args.get('b'))
-    return 'Hey this is add page'
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a,b)
+
+    return str(result)
 
 @app.route("/calc/sub")
-def sub(a,b):
+def sub():
     '''It takes 2 parameters and subtracts them
     >>> 4 - 5 = -1
     '''
     a = int(request.args.get("a"))
     b = int(request.args.get('b'))
-    return a-b
+    result = sub(a,b)
+    return str(result)
 
 @app.route("/calc/mult")
-def mult(a,b):
-     '''It takes 2 parameters and multiplies them
+def mult():
+    '''It takes 2 parameters and multiplies them
     >>> 4 * 5 = 20
     '''
+    # the args from query string do not go in the
+    # params for the fx thats only variables
     a = int(request.args.get("a"))
     b = int(request.args.get('b'))
-    return a * b
+    result = mult(a,b)
+    return str(result)
 
 @app.route("/calc/div")
-def div(a,b):
-     '''It takes 2 parameters and divides them
+def div():
+    '''It takes 2 parameters and divides them
     >>> 8/2 = 4
     '''
     a = int(request.args.get("a"))
     b = int(request.args.get('b'))
-    return a/b
+    result = div(a,b)
+    return str(result)
+
+# ----------------------------------------
+#     part 2 would so that we have less routes
+
+
+# operators = {
+#         "add": add,
+#         "sub": sub,
+#         "mult": mult,
+#         "div": div,
+#         }
+
+# @app.route("/calc/<oper>")
+# def do_math(oper):
+#     """Do math on a and b."""
+
+#     a = int(request.args.get("a"))
+#     b = int(request.args.get("b"))
+#     result = operators[oper](a, b)
+
+#     return str(result)
